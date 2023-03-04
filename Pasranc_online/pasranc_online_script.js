58fcd2b34be66347cde0a38771a2a817a5a21616
OguzK <68654301+Oguzkn@users.noreply.github.com>
2023-03-04 21:26:08 +0300

    .

--- Pasranc_online/pasranc_online_script.js
+++ Pasranc_online/pasranc_online_script.js
@@ -1,21 +1,3 @@
-
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js';
-import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCMa4_Q2TtXHZKjcgbiEFDWHQio_9-f4j0",
-    authDomain: "oguzk-1467d.firebaseapp.com",
-    databaseURL: "https://oguzk-1467d-default-rtdb.firebaseio.com",
-    projectId: "oguzk-1467d",
-    storageBucket: "oguzk-1467d.appspot.com",
-    messagingSenderId: "430568764328",
-    appId: "1:430568764328:web:51427c8682821ca7b6a39d",
-    measurementId: "G-B70BV83C80"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
 function oda_kur(prm_oda_id,prm_oyuncu_id){
     set(
         ref(db ,'oda_id/'+prm_oda_id),{
@@ -38,4 +20,3 @@ function oda_katil(prm_oda_kodu){
     }
     
 }
-export {oda_katil,oda_kur};
