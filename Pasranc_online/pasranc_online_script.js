686c07c55b04678747c5b9966a9747b36326a6e4
OguzK <68654301+Oguzkn@users.noreply.github.com>
2023-03-04 17:42:10 +0300

    a

--- Pasranc_online/pasranc_online_script.js
+++ Pasranc_online/pasranc_online_script.js
@@ -1,23 +1,4 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js';
-import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-analytics.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-firestore.js';
-import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCMa4_Q2TtXHZKjcgbiEFDWHQio_9-f4j0",
-    authDomain: "oguzk-1467d.firebaseapp.com",
-    databaseURL: "https://oguzk-1467d-default-rtdb.firebaseio.com",
-    projectId: "oguzk-1467d",
-    storageBucket: "oguzk-1467d.appspot.com",
-    messagingSenderId: "430568764328",
-    appId: "1:430568764328:web:51427c8682821ca7b6a39d",
-    measurementId: "G-B70BV83C80"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-const analytics = getAnalytics(app);
+import {getDatabase, ref, set} from "database_import_script.js";
 
 function oda_kur(prm_oda_id,prm_oyuncu_id){
 
@@ -26,7 +7,6 @@ function oda_kur(prm_oda_id,prm_oyuncu_id){
     set(ref(db ,'oda_id/'+prm_oda_id),{
         oyuncu_id:prm_oyuncu_id
     });
-
     document.getElementById("oda_kodu").value=oda_kodu;
     console.log("oda_kur calişti");
 
