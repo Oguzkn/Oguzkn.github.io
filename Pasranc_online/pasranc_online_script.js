5e71d9881acb07cb7f721f6bdc14c3dde4a46492
OguzK <68654301+Oguzkn@users.noreply.github.com>
2023-03-04 16:43:42 +0300

    a

--- Pasranc_online/pasranc_online_script.js
+++ Pasranc_online/pasranc_online_script.js
@@ -8,6 +8,29 @@ function writeUserData(userId, name, email, imageUrl) {
     });
   }
 */  
+
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js'
+import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-analytics.js'
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js'
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-firestore.js'
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCMa4_Q2TtXHZKjcgbiEFDWHQio_9-f4j0",
+    authDomain: "oguzk-1467d.firebaseapp.com",
+    databaseURL: "https://oguzk-1467d-default-rtdb.firebaseio.com",
+    projectId: "oguzk-1467d",
+    storageBucket: "oguzk-1467d.appspot.com",
+    messagingSenderId: "430568764328",
+    appId: "1:430568764328:web:51427c8682821ca7b6a39d",
+    measurementId: "G-B70BV83C80"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const analytics = getAnalytics(app);
+
 function oda_kur(prm_oda_id,prm_oyuncu_id){
 
     const db = getDatabase();
